71436ac89f44090aa93d7358cecaae5400ee0969
MinJung Kwon <mary_mary@likelion.com>
2019-06-29 10:25:14 +0900

    week8 homework

---
+++ myvenvproject/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\mjkwon\desktop\likelion\likelion week8\myvenvproject\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
